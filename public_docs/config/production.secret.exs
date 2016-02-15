e18c15c52a659e7d3fa21ba24aeef31236f72f0f
lancasp22 <lancaster_peter@hotmail.com>
2016-02-15 15:06:33 +0000

    renaming x_search_api to public_docs

--- public_docs/config/production.secret.exs
+++ public_docs/config/production.secret.exs
@@ -3,13 +3,13 @@ use Mix.Config
 # In this file, we keep production configuration that
 # you likely want to automate and keep it away from
 # your version control system.
-config :x_search_api, XSearchApi.Endpoint,
+config :public_docs, PublicDocs.Endpoint,
   secret_key_base: "j1nF9DNa3CtIi5eX16t05OfYcK72vcvcozp80LHZOc2Zcfph9d1OX8BAAAASUCYk"
 
 # Configure your database
-config :x_search_api, XSearchApi.Repo,
+config :public_docs, PublicDocs.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "postgres",
-  database: "x_search_api_prod",
+  database: "public_docs_prod",
   pool_size: 20
